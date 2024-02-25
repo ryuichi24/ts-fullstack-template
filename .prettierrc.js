7f6132af4d4cf24c6a159e09f003a8dfd9cb6f78
ryuichi24 <ryuichi.nishi24@gmail.com>
2024-02-25 19:10:49 +0900

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: false,
+  trailingComma: "all",
+  arrowParens: "always",
+  printWidth: 120,
+};
+
+module.exports = config;
