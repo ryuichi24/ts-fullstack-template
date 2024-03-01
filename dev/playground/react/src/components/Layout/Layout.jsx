95384c581e7ea990677fa4579e3d2d963817e923
ryuichi24 <ryuichi.nishi24@gmail.com>
2024-03-01 11:34:50 +0900

    feat: add playground for react

---
+++ dev/playground/react/src/components/Layout/Layout.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const Layout = (props) => {
+  const { children } = props;
+  return <div>{children}</div>;
+};
---
+++ dev/playground/react/vite.config.js
@@ -0,0 +1,24 @@
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig(({ mode }) => {
+  const envFileDir = process.cwd();
+  const env = loadEnv(mode, envFileDir, "VITE");
+  return {
+    root: "src",
+    build: {
+      sourcemap: false,
+      outDir: path.resolve(process.cwd(), "dist"),
+    },
+    server: {
+      open: "index.html",
+      port: parseInt(env.VITE_DEV_SERVER_PORT) ?? 3333,
+    },
+    resolve: {
+      alias: [{ find: "@", replacement: path.resolve(process.cwd(), "src") }],
+    },
+    plugins: [react()],
+    envPrefix: "VITE",
+  };
+});
