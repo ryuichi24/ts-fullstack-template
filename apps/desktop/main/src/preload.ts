d8e0a497c0c167a53b3d388611968ac0be5f0c4c
ryuichi24 <ryuichi.nishi24@gmail.com>
2024-03-10 16:18:25 +0900

    feat: add tsconfig for preload script

--- apps/desktop/main/src/preload.ts
+++ apps/desktop/main/src/preload.ts
@@ -1,5 +1,7 @@
+// https://github.com/electron-react-boilerplate/electron-react-boilerplate/issues/2974#issuecomment-1213008550
 import { contextBridge } from "electron";
 import { type EXPOSED } from "@ts-fullstack-template/contract";
 
 const exposed: EXPOSED = { webSocketPort: parseInt(process.argv.slice(-1)[0]) };
+
 contextBridge.exposeInMainWorld("EXPOSED", exposed);
