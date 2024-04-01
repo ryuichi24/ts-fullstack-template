cadeb08488565d78c9e37bcd8b8f0175aeb348c9
ryuichi24 <ryuichi.nishi24@gmail.com>
2024-04-02 00:09:31 +0900

    chore: clean codes

--- dev/playground/ts-react/src/App.tsx
+++ dev/playground/ts-react/src/App.tsx
@@ -1,13 +1,9 @@
 import { Layout } from "@/components";
-import { useState } from "react";
-import { useWebSocket } from "@ts-fullstack-template/use-web-socket";
 
 export const App = () => {
-  useWebSocket("wsCon");
-  const [setState, state] = useState();
   return (
     <div>
-      <Layout>App 3</Layout>
+      <Layout>App</Layout>
     </div>
   );
 };
