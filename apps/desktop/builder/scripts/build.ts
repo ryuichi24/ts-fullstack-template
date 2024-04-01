e33a7b622495df21adeb91a0b2fcb6f528475422
ryuichi24 <ryuichi.nishi24@gmail.com>
2024-04-02 00:39:17 +0900

    feat: update builder script for each platform

--- apps/desktop/builder/scripts/build.ts
+++ apps/desktop/builder/scripts/build.ts
@@ -9,23 +9,32 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 const rootPath = process.env.INIT_CWD;
 
+const isMac = process.platform === "darwin";
+const isWindows = process.platform === "win32";
+const isLinux = process.platform === "linux";
+
 if (!rootPath) {
   throw new Error("Run this script from NPM package.json script.");
 }
 
-builder.build({
-  publish: "never",
-  config: {
-    appId: "com.ryuichi24.tsfullstack",
-    productName: "TsFullStackElectronApp",
-    copyright: "Copyright © 2024 ${author}",
-    asar: true,
-    asarUnpack: "**\\*.{node,dll}",
-    directories: {
-      app: path.join(rootPath, "release", "app"),
-      output: "release/build/${version}",
-    },
-    files: ["dist", "node_modules", "package.json"],
+let builderConfig: builder.Configuration = {
+  appId: "com.ryuichi24.tsfullstack",
+  productName: "TsFullStackElectronApp",
+  copyright: "Copyright © 2024 ${author}",
+  asar: true,
+  asarUnpack: "**\\*.{node,dll}",
+  directories: {
+    app: path.join(rootPath, "release", "app"),
+    output: "release/build/${version}",
+  },
+  files: ["dist", "node_modules", "package.json"],
+  npmRebuild: false,
+  electronVersion: devDependencies.electron.replace("^", ""),
+};
+
+if (isMac) {
+  builderConfig = {
+    ...builderConfig,
     mac: {
       // icon: "dist/assets/images/app-icon.png",
       target: {
@@ -33,7 +42,42 @@ builder.build({
         arch: ["arm64", "x64"],
       },
     },
-    npmRebuild: false,
-    electronVersion: devDependencies.electron.replace("^", ""),
-  },
+    dmg: {
+      contents: [
+        {
+          x: 130,
+          y: 220,
+        },
+        {
+          x: 410,
+          y: 220,
+          type: "link",
+          path: "/Applications",
+        },
+      ],
+    },
+  };
+}
+
+if (isWindows) {
+  builderConfig = {
+    ...builderConfig,
+    win: {
+      target: ["nsis"],
+    },
+  };
+}
+
+if (isLinux) {
+  builderConfig = {
+    ...builderConfig,
+    linux: {
+      target: ["AppImage"],
+    },
+  };
+}
+
+builder.build({
+  publish: "never",
+  config: builderConfig,
 });
