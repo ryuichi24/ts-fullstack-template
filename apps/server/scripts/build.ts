455e19785747e9d459b7dc4eae7527e76d28c0ef
ryuichi24 <ryuichi.nishi24@gmail.com>
2024-04-06 17:30:16 +0900

    feat: setup server proj

---
+++ apps/server/scripts/build.ts
@@ -0,0 +1,13 @@
+import { buildAsEsNode } from "@ts-fullstack-template/esbuild-config/node";
+
+buildAsEsNode({
+  entryPoints: ["./src/index.ts"],
+  outdir: "./dist",
+  tsconfig: "./tsconfig.json",
+  banner: {
+    // for common js imported from third-party libs
+    js: 'import { createRequire } from "module"; import url from "url"; const require = createRequire(import.meta.url); const __filename = url.fileURLToPath(import.meta.url); const __dirname = url.fileURLToPath(new URL(".", import.meta.url));',
+  },
+  external: [],
+  sourcemap: process.env.NODE_ENV === "debug" ? true : false,
+});
